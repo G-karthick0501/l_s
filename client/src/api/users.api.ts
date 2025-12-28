9044a123ecea29678f61115311a10bf86bfb4aa5
G-karthick0501 <karthick.venger@gmail.com>
2025-12-28 22:43:05 +0530

    added user add and delete functionality

--- client/src/api/users.api.ts
+++ client/src/api/users.api.ts
@@ -1,3 +1,8 @@
 import api from "./axios";
 
 export const getUsers = () => api.get("/users");
+
+export const createUser = (data: { name: string; email: string }) =>
+  api.post("/users", data);
+
+export const deleteUser = (id: number) => api.delete(`/users/${id}`);
